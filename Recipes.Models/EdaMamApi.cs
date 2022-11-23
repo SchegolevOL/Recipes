87daad4306aec73146e6e0ed782f37787325d345
SchegolevOL <92328776+SchegolevOL@users.noreply.github.com>
2022-11-23 22:03:51 +0500

    add EdaMam

---
+++ Recipes.Models/EdaMamApi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Recipes.Models
+{
+    public static class EdaMamApi
+    {
+        public static async Task<Root> GetRecipesAsync(string quary)
+        {
+            var appId = "45d8b6fa";
+            var appKey = "bf72c7e4fec04d45aec7471754063466";
+            var url = $"https://api.edamam.com/api/recipes/v2?app_id={appId}&app_key={appKey}&q={quary}&type=public";
+            using var client = new HttpClient();
+            var stream = await client.GetStreamAsync(url);
+            return await JsonSerializer.DeserializeAsync<Root>(stream);
+        }
+    }
+}
