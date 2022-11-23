555ff47180bdab0c212d530a461ccae97878f912
SchegolevOL <92328776+SchegolevOL@users.noreply.github.com>
2022-11-23 22:04:17 +0500

    class RecipesConverter

---
+++ Recipes.App/Lib/RecipesConverter.cs
@@ -0,0 +1,28 @@
+﻿using Recipes.App.Models;
+using Recipes.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipes.App.Lib
+{
+    public static class RecipesConverter
+    {
+
+        public static async IAsyncEnumerable<Models.Recipe> ConvertAsync(Root recipes)
+        {
+            foreach (var recipe in recipes.Hits.Select(hit => hit.Recipe))
+            {
+                yield return await Task.Run(() => new Models.Recipe
+                {
+                    Title = recipe.Label,
+                    Ingredients = new ObservableCollection<string>(recipe.IngredientLines),
+                    Image = recipe.Images.THUMBNAIL.Url
+                });
+            }
+        }
+    }
+}
