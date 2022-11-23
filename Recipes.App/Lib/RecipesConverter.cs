8dd7be76f669d8a02ee7b917b19a96f431732559
SchegolevOL <92328776+SchegolevOL@users.noreply.github.com>
2022-11-23 22:42:46 +0500

    update

--- Recipes.App/Lib/RecipesConverter.cs
+++ Recipes.App/Lib/RecipesConverter.cs
@@ -4,25 +4,21 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-
-namespace Recipes.App.Lib
+using Recipes.Models;
+using Recipe = Recipes.App.Models.Recipe;
+namespace Recipes.App.Lib;
+public static class RecipesConverter
 {
-    public static class RecipesConverter
+    public static async Task<IEnumerable<Recipe>> ConvertAsync(Root recipes)
     {
-
-        public static async IAsyncEnumerable<Models.Recipe> ConvertAsync(Root recipes)
-        {
-            foreach (var recipe in recipes.Hits.Select(hit => hit.Recipe))
+        return await Task.Run(() => recipes.Hits
+            .Select(hit => hit.Recipe)
+            .Select(recipe => new Recipe
             {
-                yield return await Task.Run(() => new Models.Recipe
-                {
-                    Title = recipe.Label,
-                    Ingredients = new ObservableCollection<string>(recipe.IngredientLines),
-                    Image = recipe.Images.THUMBNAIL.Url
-                });
-            }
-        }
+                Title = recipe.Label,
+                Ingredients = new ObservableCollection<string>(recipe.IngredientLines),
+                Image = recipe.Images.THUMBNAIL.Url
+            }));
     }
 }
--- Recipes.App/ViewModels/MainWindowViewModel.cs
+++ Recipes.App/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,14 @@
 ﻿using Recipes.App.Commands;
+using Recipes.App.Lib;
 using Recipes.App.Models;
+using Recipes.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Recipe = Recipes.App.Models.Recipe;
 namespace Recipes.App.ViewModels
 {
     public class MainWindowViewModel: BaseNotification
@@ -21,10 +23,19 @@ namespace Recipes.App.ViewModels
 
         public MainWindowViewModel()
         {
-            Recipes = new ObservableCollection<Recipe>();
+            Recipes = new ObservableCollection<Models.Recipe>();
             Recipe = new Recipe();
+            CommandSearch = new LambdaCommand(_ => true, async _=> await GetData());
         }
-
+        private async Task GetData()
+        {
+            var r = await EdaMamApi.GetRecipesAsync(Search);
+            var t = await RecipesConverter.ConvertAsync(r);
+            foreach (var item in t)
+            {
+                Recipes.Add(item);
+            }
+        }
         private string _search;
         public string Search { get => _search; set => SetField(ref _search, value); }
 
